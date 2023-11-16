6b2779c1d645ac8f4e415e1ba3946f14bf619a77
saishadow5473 <128263691+saishadow5473@users.noreply.github.com>
2023-11-16 16:36:08 +0530

    Update test.py

--- test.py
+++ test.py
@@ -1,49 +1,24 @@
 from selenium import webdriver
-from selenium.webdriver.common.by import By
-from selenium.webdriver.support.ui import WebDriverWait
-from selenium.webdriver.support import expected_conditions as EC
+from selenium.common.exceptions import NoSuchElementException
 
-def main():
-    # Set up the Selenium WebDriver with options
-    options = webdriver.ChromeOptions()
-    # Comment the next line if you want to see the browser window
-    options.add_argument('--headless')  # Use this if you're running headless
-    # Actual path to Chrome binary (change this to the path on your machine)
-    options.binary_location = '/usr/bin/google-chrome'
+# Set up the web driver (make sure you have the appropriate webdriver installed, e.g., ChromeDriver)
+driver = webdriver.Chrome()
 
-    # Initialize the Chrome WebDriver
-    driver = webdriver.Chrome(options=options)
+# Open the URL
+url = "https://kioskportal.indiahealthlink.com/portalm"
+driver.get(url)
 
-    try:
-        # Open the specified URL
-        target_url = "https://kioskportal.indiahealthlink.com/portalm/Account/Login"
-        driver.get(target_url)
+try:
+    # Find the element using XPath
+    element = driver.find_element_by_xpath("/html/body/div/div[3]/div[2]/div[3]/div[2]/div/div/div/div")
+    
+    # If the element is found, print "URL reached"
+    print("URL reached")
 
-        # Check if the URL was successfully reached
-        if "Login" in driver.title:
-            print(f"URL '{target_url}' successfully reached")
+except NoSuchElementException:
+    # If the element is not found, print "URL not reached"
+    print("URL not reached")
 
-            # Verify the presence of the element with the given XPath
-            xpath_to_verify = "/html/body/div/div[3]/div[2]/div[3]/div[2]/div/div/div/div"
-            try:
-                element = WebDriverWait(driver, 10).until(
-                    EC.presence_of_element_located((By.XPATH, xpath_to_verify))
-                )
-                print(f"XPath {xpath_to_verify} verified. URL reached.")
-            except Exception as e:
-                print(f"XPath {xpath_to_verify} not found. URL didn't reach as expected.")
-
-        else:
-            print(f"URL '{target_url}' didn't reach as expected")
-
-        # Add additional interactions with the web page here if needed
-
-    except Exception as e:
-        print(f"An error occurred while opening the URL: {str(e)}")
-
-    finally:
-        # Close the browser window
-        driver.quit()
-
-if __name__ == "__main__":
-    main()
+finally:
+    # Close the browser window
+    driver.quit()
