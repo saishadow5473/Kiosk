7e910e2e1f37a7b5cd7777170f7a71b4963f8bf9
saishadow5473 <128263691+saishadow5473@users.noreply.github.com>
2023-11-20 15:56:33 +0530

    Update test.py

--- test.py
+++ test.py
@@ -3,6 +3,7 @@ from selenium.webdriver.common.by import By
 from selenium.webdriver.support.ui import WebDriverWait
 from selenium.webdriver.support import expected_conditions as EC
 import time
+import pyautogui
 
 def main():
     # Set up the Selenium WebDriver with options
